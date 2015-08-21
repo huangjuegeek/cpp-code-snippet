1f96bfa7bad57a661e399bb472b3d571b1e09266
Jue Huang <hjdick@163.com>
2015-08-21 18:50:06 +0800

    Create io01.cpp

---
+++ io01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// 用户在控制台中输入一串整数和任意数目的空格，这些整数和空格位于同一行。
+// 当用户按下"Enter"键时结束输入，输出所有整数的和
+int main()
+{
+	int a, sum = 0;
+	while (cin >> a)
+	{
+		sum = sum + a;
+		if (cin.get() == '\n')
+			break;
+	}
+	cout << sum << endl;
+	return 0;
+}
